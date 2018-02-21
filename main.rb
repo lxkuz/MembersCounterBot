0d4b397da4d616cc9b54bcde83999a864461bb6c
Alexey Kuznetsov <alexey.kuznetsov@import2.com>
2018-02-21 12:06:49 +0500

    add rescue block

--- main.rb
+++ main.rb
@@ -24,16 +24,16 @@ class EventManager
     votes(chat_id).keys.count || 0
   end
 
-  def status(chat_id)
-    if started(chat_id)
-      "Event: #{name(chat_id)}, limit: #{limit(chat_id)}, available: #{limit(chat_id)- value(chat_id)}"
-    else
-      index = 0
-      "Event: #{name(chat_id)} \n" +
-        "Members: \n" + (votes(chat_id).values || []).each do |name|
-          index += 1
-          "#{index}) #{name}"
-      end.join("\n")
+  def status(chat_id, full = false)
+    message = "Event: #{name(chat_id)}, limit: #{limit(chat_id)}, available: #{limit(chat_id)- value(chat_id)}"
+
+    if full || !started(chat_id)
+      idx = 0
+      members = (votes(chat_id).values || []).each do |name|
+        idx = idx + 1
+        "#{idx.to_s}. #{name}"
+      end
+      "#{message}\nMembers:\n#{members.join("\n")}"
     end
   end
 
@@ -83,25 +83,30 @@ end
 Telegram::Bot::Client.run(token) do |bot|
   @event_manager = EventManager.new
   bot.listen do |message|
-    case message.text
-      when /\/event\s*(\S*)\s*(\S*)/
-        name = $1
-        limit = $2
-        if limit.empty?
-          response = 'Example: /event Hockey 10'
-        else
-          @event_manager.setup(name, limit, message.chat.id)
-          response = @event_manager.status(message.chat.id)
-        end
-        bot.api.sendMessage(chat_id: message.chat.id, text: response)
-      when '/status'
-        bot.api.sendMessage(chat_id: message.chat.id, text: @event_manager.status(message.chat.id))
-      when '+'
-        @event_manager.inc(message.chat.id, message.from)
-        bot.api.sendMessage(chat_id: message.chat.id, text: @event_manager.status(message.chat.id))
-      when '-'
-        @event_manager.dec(message.chat.id, message.from)
-        bot.api.sendMessage(chat_id: message.chat.id, text: @event_manager.status(message.chat.id))
+    begin
+      case message.text
+        when /\/event\s*(\S*)\s*(\S*)/
+          name = $1
+          limit = $2
+          if limit.empty?
+            response = 'Example: /event Hockey 10'
+          else
+            @event_manager.setup(name, limit, message.chat.id)
+            response = @event_manager.status(message.chat.id)
+          end
+          bot.api.sendMessage(chat_id: message.chat.id, text: response)
+        when '/status'
+          bot.api.sendMessage(chat_id: message.chat.id, text: @event_manager.status(message.chat.id, true))
+        when '+'
+          @event_manager.inc(message.chat.id, message.from)
+          bot.api.sendMessage(chat_id: message.chat.id, text: @event_manager.status(message.chat.id))
+        when '-'
+          @event_manager.dec(message.chat.id, message.from)
+          bot.api.sendMessage(chat_id: message.chat.id, text: @event_manager.status(message.chat.id))
+      end
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
     end
   end
 end
